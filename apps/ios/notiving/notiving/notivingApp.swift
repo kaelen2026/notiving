26e312556efaa654b3f826a8a3e71ccd3b2c2f7f
kaelen <kaelen.hou@gmail.com>
2026-04-21 12:05:03 +0800

    feat(ios): add ios app
    - scaffold SwiftUI project with Xcode
    - configure asset catalogs and app icons
    - add ContentView and app entry point

---
+++ apps/ios/notiving/notiving/notivingApp.swift
@@ -0,0 +1,17 @@
+//
+//  notivingApp.swift
+//  notiving
+//
+//  Created by sam on 2026/4/21.
+//
+
+import SwiftUI
+
+@main
+struct notivingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
